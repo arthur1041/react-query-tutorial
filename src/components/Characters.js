fd06d3086b012ae5f3772e241b04c723e724b4f0
Arthur Cruz <arthur.silva1041@gmail.com>
2022-05-02 08:52:59 -0300

    fetching data without react query

---
+++ src/components/Characters.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+const Characters = () => {
+  
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    setCharacters(data.results);
+  }
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div>
+      {characters.map(character => (
+        <div key={character.id} >{character.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Characters;
